690a17f72bd874ffaa7025c747c9773916cb2231
karan <rathodkaransingh05@gmail.com>
2026-09-26 11:37:23 +0530

    Modified User Dao

--- src/main/java/com/ptms/app/dao/UserDao.java
+++ src/main/java/com/ptms/app/dao/UserDao.java
@@ -4,23 +4,15 @@ import com.ptms.app.model.User;
 
 import java.sql.SQLException;
 import java.util.List;
-
 public interface UserDao {
 
     int insert(User user) throws SQLException;
-
     User findById(int id) throws SQLException;
-
     User findByUsername(String username) throws SQLException;
-
     List<User> findAll() throws SQLException;
-
     List<User> findByRole(String roleName) throws SQLException;
-
     List<User> searchByName(String keyword) throws SQLException;
-
     boolean update(User user) throws SQLException;
-
     boolean delete(int id) throws SQLException;
 
     /**
